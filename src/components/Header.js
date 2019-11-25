db111847ba810e53cf34632fa910a7f6f2caaf0a
Frida <friederike.suhlmann@gmail.com>
2019-11-25 15:47:51 +0100

    Add user issue profile_details

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,18 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import MenuIcon from '@material-ui/icons/Menu'
-import './styles.scss'
+import '../styles/styles.scss'
 
 export default function Header() {
 	return (
 		<div>
-			<AppBar position="static">
+			<AppBar position="fixed">
 				<Toolbar className="header">
 					<MenuIcon fontSize="large" />
 					<h3 className="header__title">page title</h3>
 					<img
-						src={require('./images/Logo.png')}
+						src={require('../images/Logo.png')}
+						alt=""
 						className="header__logoicon"
 					/>
 				</Toolbar>
